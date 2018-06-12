9ef4ea154e824ca5ea34d05e8ff775a732312a3f
Pavlo Kostrytsia <pahanela@gmail.com>
2018-06-13 00:57:22 +0400

    Solved #6

--- 6. ZigZag Conversion/solution.c
+++ 6. ZigZag Conversion/solution.c
@@ -1,14 +1,39 @@
 char* convert(char* s, int numRows) {
     int len = 0;
-    int i = 0;
-    while (s[i++] != '\0')
+    int numCols = 0;
+    // find the string length
+    while (s[len] != '\0')
         len++;
-    char* sOut = malloc(sizeof(char) * len);
-    i = 0;
-    while(i < len){
-        if (!(i % (numRows * 2 - 2)))
-            sOut[i] = s[i];
-        i++;
+    if (len == 0 || len == 1 || numRows == 1)
+        return s;
+    // allocate space for the output buffer
+    char* sOut = malloc(sizeof(char) * (len + 1));
+    sOut[len] = '\0';
+    // find number of columns
+    numCols = len / (numRows * 2 - 2) * 2 + 2;
+    // allocate buffer for calculation matrix and initialize it to special value
+    char matrix[numRows][numCols];
+    memset(matrix, '~', numRows * numCols * sizeof(char));
+    // populate matrix with zigzag
+    for (int i = 0, row = 0, col = 0; i < len; i++){
+        if ((row < numRows - 1 && (col % 2) == 0) || (row == 0))
+            matrix[row++][col] = s[i];
+        else if (row == numRows - 1)
+            matrix[row--][col++] = s[i];
+        else if (row > 1 && (col % 2) == 1)
+            matrix[row--][col] = s[i];
+        else if (row == 1 && (col % 2) == 1)
+            matrix[row--][col++] = s[i];
+    }
+    // read matrix by rows
+    int k = 0;
+    for (int i = 0; i < numRows; i++){
+        for (int j = 0; j < numCols && k < len; j++){
+            if (matrix[i][j] != '~'){
+                sOut[k] = matrix[i][j];
+                k++;
+            }
+        }
     }
     return sOut;
 }
