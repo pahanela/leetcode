9d6e600536e8f095161eb5183b375338d29941cc
pahanela <pahanela@gmail.com>
2022-06-09 19:51:44 -0700

    Add solution for 003 in cpp

--- 003.LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cpp
+++ 003.LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int i = 0;
+        int start = 0;
         int maxLen = 0;
         int counter = 0;
-        unordered_set <char> chars;
+        unordered_map <char, int> chars;
         for(char& c : s) {
             if (chars.count(c)) {
                 if (counter > maxLen) {
                     maxLen = counter;
-                    counter = 0;
                 }
+                if (chars[c] > start)
+                    start = chars[c];
+                counter = i - start;
+                chars[c] = i;
             }
-            else
+            else {
+                chars.insert({c, i});
                 counter++;
-            
+            }
+            i++;
         }
+        if (counter > maxLen)
+            maxLen = counter;
         return maxLen;
     }
 };
-
 
