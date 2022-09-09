15808048cbdbd569b222015e3fe14f9fd887885e
pahanela <pahanela@gmail.com>
2022-09-08 21:40:41 -0700

    Add optimized cpp solution for 015

--- 015.3Sum/015.3Sum.cpp
+++ 015.3Sum/015.3Sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> rtrn;
+        std::vector<vector<int>> rtrn;
+        std::sort(nums.begin(), nums.end());
+        int n = nums.size();
         for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i + 1; j < nums.size(); ++j) {
-                for (int k = j + 1; k < nums.size(); ++k) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        vector<int> triplet = {nums[i], nums[j], nums[k]};
-                        sort(triplet.begin(), triplet.end());
-                        if (find(rtrn.begin(), rtrn.end(), triplet) == rtrn.end())
-                            rtrn.push_back(triplet);
-                    }
+            if (nums[i] > 0)
+                break;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < n && j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    rtrn.push_back({nums[i],nums[j],nums[k]});
+                    while(j! = n-1 && nums[j] == nums[j+1])
+                        j++;
+                    while(k! = 0 && nums[k] == nums[k-1])
+                        k--;
+                    j++;
+                    k--;
+                }
+                else if (-nums[i] < (nums[j] + nums[k])) {
+                    while (k != 0 && nums[k] == nums[k - 1])
+                        k--;
+                    k--;
+                }
+                else {
+                    while (j != n - 1 && nums[j] == nums[j + 1])
+                        j++;
+                    j++;
                 }
             }
+            while (i != n - 1 && nums[i] == nums[i + 1])
+                i++;
         }
         return rtrn;
     }
