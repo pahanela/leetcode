a6b81ee648b694f4de8fd08f0f99712e9312bf6e
pahanela <pahanela@gmail.com>
2024-01-22 17:42:10 -0800

    Add cpp solution for 933

---
+++ 933.NumberOfRecentCalls/933.NumberOfRecentCalls.cpp
@@ -0,0 +1,23 @@
+class RecentCounter {
+public:
+    std::queue<int> requests;
+
+    RecentCounter() {
+        requests.empty();
+    }
+    
+    int ping(int t) {
+        requests.push(t);
+        while(requests.front() < t - 3000)
+            requests.pop();
+
+        return requests.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+
