720889ac556fb917b13e6b1963a6c14dee495e01
Pavlo Kostrytsia <pahanela@gmail.com>
2024-09-09 21:30:45 -0700

    Add cpp solution for 236

--- 236.LowestCommonAncestorOfABinaryTree/236.LowestCommonAncestorOfABinaryTree.cpp
+++ 236.LowestCommonAncestorOfABinaryTree/236.LowestCommonAncestorOfABinaryTree.cpp
@@ -10,43 +10,28 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* rtrnNode = nullptr;
-        bool pFlag = false;
-        bool qFlag = false;
-        traverseTree(root, p, q, pFlag, qFlag, rtrnNode);
-
-        return rtrnNode;
+        TreeNode* lca = nullptr;
+        findElements(root, p, q, false, lca);
+        return lca;
     }
 
-    void traverseTree(TreeNode* root, TreeNode* p, TreeNode* q, bool &pFound, bool &qFound, TreeNode* &lca) {
-
+    // Function to traverse the tree from root in search of both elements, p and q
+    // The flagsFromAbove variable passes search results from top to bottom, to indicate when to stop
+    uint8_t findElements(TreeNode* root, TreeNode* p, TreeNode* q, uint8_t flagsFromAbove, TreeNode* &lca) {
+        unsigned int flags = 0b00;
         if (root) {
-            
             if (root->val == p->val)
-                pFound = true;
-
+                flags |= 0b10;
             if (root->val == q->val)
-                qFound = true;
-
-            if (!(pFound && qFound)) {
-                traverseTree(root->left, p, q, pFound, qFound, lca);
-                traverseTree(root->right, p, q, pFound, qFound, lca);
-            }
-
-            if (pFound && qFound && !lca)
+                flags |= 0b01;
+            if ((flags | flagsFromAbove) != 0x11)
+                flags |= \
+                    findElements(root->left, p, q, flags | flagsFromAbove, lca) | \
+                    findElements(root->right, p, q, flags | flagsFromAbove, lca);
+            if (flags == 0b11 && lca == nullptr)
                 lca = root;
-        
         }
-      
-        return;
+        return flags;
     }
-
-    bool findElement
-    
 };
 
-
-// check if both are found then set the Node
-// if none are found keep searching left and right
-// if one is found keep searching for another one
-//
